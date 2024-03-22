b58718131982ece909b6e3bebedf9dc22667d0cf
AYVolkov <maloletka2222@mail.ru>
2024-03-22 17:21:24 +0300

    add new tab

--- lib/src/feature/home/widget/home_screen.dart
+++ lib/src/feature/home/widget/home_screen.dart
@@ -1,16 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:hs_conclusion/src/feature/conclusion/widget/conclusion_screen.dart';
+import 'package:hs_conclusion/src/feature/conclusion/widget/replacement_screen.dart';
 
 /// {@template sample_page}
 /// SamplePage widget
 /// {@endtemplate}
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   /// {@macro sample_page}
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _tabInddex = 0;
+  @override
   Widget build(BuildContext context) => Scaffold(
         backgroundColor: Theme.of(context).colorScheme.background,
-        body: const SafeArea(child: ConclusionScreen()),
+        body: SafeArea(
+          child: IndexedStack(
+            index: _tabInddex,
+            children: const [
+              ConclusionScreen(),
+              ReplacementScren(),
+            ],
+          ),
+        ),
+        bottomNavigationBar: NavigationBar(
+          selectedIndex: _tabInddex,
+          onDestinationSelected: (int index) {
+            setState(() {
+              _tabInddex = index;
+            });
+          },
+          destinations: const [
+            NavigationDestination(
+              icon: Icon(Icons.list),
+              label: 'Ввывод',
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.list_alt),
+              label: 'Замена',
+            ),
+          ],
+        ),
       );
 }
